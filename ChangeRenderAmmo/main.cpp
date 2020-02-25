3feb0840b1370712922686a044c57f4764306da5
4el0ve4ik <aask7806@gmail.com>
2020-02-25 21:22:23 +0300

    Update main.cpp

--- ChangeRenderAmmo/main.cpp
+++ ChangeRenderAmmo/main.cpp
@@ -1,9 +1,9 @@
 #include <Windows.h>
 #include <process.h>
-
+//меняем аргументы sprintf местами
 void Thread(void*) {
-	*reinterpret_cast<unsigned char*>(0x589477) = 0x50;
-	*reinterpret_cast<unsigned char*>(0x589478) = 0x51;
+	*reinterpret_cast<unsigned char*>(0x589477) = 0x50; // push eax
+	*reinterpret_cast<unsigned char*>(0x589478) = 0x51; // push ecx
 }
 
 BOOL APIENTRY DllMain(HMODULE, DWORD  fdwReason, LPVOID) {
