ebb6665a1c3150b1b5facdea3bd690fe58b666af
4el0ve4ik <aask7806@gmail.com>
2020-04-21 10:38:02 +0300

    Update main.cpp

--- ChangeRenderAmmo/main.cpp
+++ ChangeRenderAmmo/main.cpp
@@ -1,9 +1,13 @@
 #include <Windows.h>
 #include <process.h>
+
 //меняем аргументы sprintf местами
 void Thread(void*) {
-	*reinterpret_cast<unsigned char*>(0x589477) = 0x50; // push eax
-	*reinterpret_cast<unsigned char*>(0x589478) = 0x51; // push ecx
+	unsigned long oldProt;
+	VirtualProtect((void*)0x589477, 2, PAGE_EXECUTE_READWRITE, &oldProt);
+	*reinterpret_cast<unsigned char*>(0x589477) = 0x50;// push eax
+	*reinterpret_cast<unsigned char*>(0x589478) = 0x51;// push ecx
+	VirtualProtect((void*)0x589477, 2, oldProt, NULL);
 }
 
 BOOL APIENTRY DllMain(HMODULE, DWORD  fdwReason, LPVOID) {
